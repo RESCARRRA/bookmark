2cba8f602bec1cc53e5b965e7bd9a4024a1a4645
Rescarra <ryn.b.escarra@gmail.com>
2018-03-01 14:38:34 -0600

    inital

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Jumbotron = ({ children }) =>
+  <div className="header well-lg">
+    {children}
+  </div>;
+
+export default Jumbotron;
+
+// style={{ height: 300 }} 
